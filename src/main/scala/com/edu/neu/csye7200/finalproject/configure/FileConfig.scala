1058094322262489594e402988f55fc44e13485f
胡霄 <hu.xiao4@husky.neu.edu>
2019-04-18 17:05:08 -0400

    small test of aws

--- src/main/scala/com/edu/neu/csye7200/finalproject/configure/FileConfig.scala
+++ src/main/scala/com/edu/neu/csye7200/finalproject/configure/FileConfig.scala
@@ -11,9 +11,9 @@ package com.edu.neu.csye7200.finalproject.configure
 
 object FileConfig {
   val dataDir = "input/"
-  val ratingFile = dataDir + "ratings.csv"
+  val ratingFile = dataDir + "ratings_small.csv"
   val movieFile = dataDir + "movies_metadata.csv"
-  val linkFile = dataDir + "links.csv"
+  val linkFile = dataDir + "links_small.csv"
   val keywordsFile = dataDir + "keywords.csv"
   val creditFIle = dataDir + "credits.csv"
 }
